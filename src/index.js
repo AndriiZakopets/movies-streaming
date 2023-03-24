5f942094c5b1526023c46adf9b19196930baeafb
Andrii Zakopets <snailz839@gmail.com>
2023-03-24 03:31:44 +0200

    fix public server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import path from 'path';
+import url from 'url';
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../public/')));
